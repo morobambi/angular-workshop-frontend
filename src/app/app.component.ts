880047e8fbbd802c0ff437c185cde0fd922cbae3
Tomas Bambas <tomas.bambas@morosystems.cz>
2018-09-15 15:27:17 +0200

    my service pouziti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MyService} from './my.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'moro-projekt';
+export class AppComponent implements OnInit {
+  title = '';
+
+  constructor(private myService: MyService) {
+
+  }
+
+  ngOnInit(): void {
+    this.title = this.myService.getApplicationTitle();
+  }
 }
--- src/app/my.service.ts
+++ src/app/my.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class MyService {
 
   constructor() { }
+
+  getApplicationTitle(): string {
+    return 'titulek';
+  }
 }
