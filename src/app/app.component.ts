aa5ab9c47461d5da35d35a617a2188e5b94290f2
Tomas Bambas <tomas.bambas@morosystems.cz>
2018-09-16 12:39:37 +0200

    Úkol č. 1: Kalkulačka, řešení

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,36 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
+  input1: number;
+  input2: number;
+  operation: string;
+  result: number;
+
   constructor() {}
+
+  countResult() {
+    switch (this.operation) {
+      case '+': {
+        this.result = this.input1 + this.input2;
+        break;
+      }
+      case '-': {
+        this.result = this.input1 - this.input2;
+        break;
+      }
+      case '*': {
+        this.result = this.input1 * this.input2;
+        break;
+      }
+      case '/': {
+        this.result = this.input1 / this.input2;
+        break;
+      }
+      default: {
+        throw new Error('Invalid operation: ' + this.operation);
+      }
+    }
+  }
 
   ngOnInit(): void {
   }
