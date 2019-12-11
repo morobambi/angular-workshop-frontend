f733b98b00a5c714aa47d5d34ca94dff820b16ee
Tomas Bambas <tomas.bambas@morosystems.cz>
2019-12-11 16:07:04 +0100

    my pipe použití

--- src/app/my.pipe.ts
+++ src/app/my.pipe.ts
@@ -1,12 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'my'
+  name: 'reverse'
 })
 export class MyPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any, ...args: any): any {
+    return value ? value.split('').reverse().join('') : value;
   }
-
 }
